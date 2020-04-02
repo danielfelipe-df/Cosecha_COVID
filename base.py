b2e25715af943fc82af24b5f11a861253cc6f901
Daniel Ortiz <dfortizc1@github.com>
2020-04-02 12:28:00 -0500

    Se añaden las mofidicaciones correspondientes a la primera presentación del PDF. Se modifican los archivos .py para una mejor presentación. Se define la constante k=1.3345e-8 para la normalización deseada. Se agrega, como comentario, las líneas de código correspondientes a la entrada de los casos importados en los lugares y días correspondientes.

--- base.py
+++ base.py
@@ -1,32 +1,77 @@
 import network as net
 import SIRODE as SIR
+import ZipfDistance as ZD
 import networkx as nx
 import numpy as np
 import random as ran
 
+#Leo le archivo donde está la información de población y ubicación.
+my_data = np.genfromtxt("BaseCol_Dep.csv", delimiter=';', dtype=None, encoding='utf-8-sig')
+
+#Defino el número de nodos
+N = len(my_data)
+
+#Guardo los atributos de nombre y población, y la propiedad de posición
+name = {}
+pop = {}
+lat = []
+lon = []
+for i in range(N):
+    name[i] = my_data[i][0]
+    pop[i] = my_data[i][1]
+    lat.append(my_data[i][2])
+    lon.append(my_data[i][3])
+
+#Creo la red con el número de nodos adecuados
+G = nx.empty_graph(N)
+
+#Creo los archivos donde se van a imprimir los distancias y los Zipf
+file1 = open("vinculos.csv", "w")
+file2 = open("zipf.csv", "w")
+
+#Imprimo la distancia, el Zipf y agrego el vínculo a la red
+kk = 1.3345e-8
+for i in range(N):
+    for j in range(i+1,N):
+        #Hallo la distancia
+        dist = ZD.coordinatetodistance(lat[i],lon[i],lat[j],lon[j])
+        #Hallo el zipf
+        zipf = ZD.ZipfGravity(kk,pop[i],pop[j],dist)
+        #Imprimo la distancia y el zipf en los archivos correspondientes
+        file1.write(str(name[i]) + '\t' + str(name[j]) + '\t' + str(dist) + '\n')
+        file2.write(str(name[i]) + '\t' + str(name[j]) + '\t' + str(zipf) + '\n')
+        #Agrego el vínculo con su peso
+        G.add_edge(i, j, weight=float(zipf))
+file1.close()
+file2.close()
+
+#Obtener la matriz de pesos
+matrix = nx.to_numpy_matrix(G)
+
+#Hallo la suma de los pesos alrededor de un nodo
+sumv = []
+for i in range(N):
+    sumv.append(np.sum(matrix[i]))
+aux = sumv[:]
+sumv.sort()
+
+#Los imprimo
+file1 = open("Suma_pesos.csv", "w")
+for i in range(N):
+    for j in range(N):
+        if(sumv[i] == aux[j]):
+            file1.write(name[j] + '\t' + str(sumv[i]) + '\n')
+            break
+file1.close()
+
 #Defino las variables de las personas en la red
-S0 = 59999970
-E0 = 0
-I0 = 30
-R0 = 0
+I0 = 1
 beta0 = 1.95
 gamma0 = 0.8
 sigma0 = 0.22
 
-#Defino las variables de la red
-N = 30
-param = 30
-
-#Creo el grafo
-G = nx.watts_strogatz_graph(N,param,0,876)
-
-#Genero la matriz de adjacencia como una matriz de numpy para editar
-matrix = nx.to_numpy_matrix(G)
-#Hago que sea una matriz pesada
-ran.seed(636748)
-for i in range(N*N):
-    if(matrix.item(i) != 0):
-        matrix.itemset(i, ran.randint(1,10))
+#Agrego el nombre de cada nodo como un atributo de cada nodo
+nx.set_node_attributes(G, name, 'Nombre')
         
 #Defino el diccionario que me va a dar la propiedad de Susceptibles, Expuesto, Infectado y Recuperado
 sus = {}
@@ -34,22 +79,14 @@ exp = {}
 inf = {}
 rec = {}
 for i in range(N):
-    sus[i] = 0
+    sus[i] = pop[i]
     exp[i] = 0
     inf[i] = 0
     rec[i] = 0
-#Defino la semilla para el número aleatorio de susceptibles
-ran.seed(7645)
-#Guardo cada Susceptible en algún nodo
-for i in range(S0):
-    #sus[ran.randint(0,N-1)] += 1
-    sus[int(N*ran.gauss(0.5,0.17))%N] += 1
-print("Susceptibles: ", sus)
-#Hago lo mismo para los Infectados
-#ran.seed(6738)
-for i in range(I0):
-    inf[i] += 1
-print("Infectados: ", inf)
+    
+#Cundinamarca es el nodo número 2, entonces le sumo un infectado
+inf[2] += 1
+sus[2] -= 1
 
 #Guardo los Susceptibles, Expuestos, Infectados y Recuperados como atributos del nodo
 nx.set_node_attributes(G, sus, 'Susceptibles')
@@ -60,41 +97,52 @@ nx.set_node_attributes(G, rec, 'Recuperados')
 #Le doy una forma circular para poderlo ver mejor
 pos = nx.circular_layout(G)
 
+print(name)
+
+#infected = np.genfromtxt("casos_importados.csv", delimiter='\t', dtype=None, encoding='utf-8-sig')
+
 #Hacer el ciclo sobre T pasos de tiempo
-T = 100
-dt = 1
+T = 2
+dt = 100
 ti = 0
 tf = ti + dt
 for i in range(T):
+    '''
+    if(i<30):
+        #Obtengo el atributo de infectados y lo modifico
+        inf = nx.get_node_attributes(G, 'Infectados')
+        for j in range(1,len(infected)):
+            if(int(infected[j][0]) == i):
+                inf[int(infected[j][2])] += int(infected[j][4])
+                #Los vuelvo a añadir a la red
+        nx.set_node_attributes(G, inf, 'Infectados')
+    '''
+            
     #Obtengo los diccionarios de cada tipo de persona
     sus = nx.get_node_attributes(G, 'Susceptibles')
     exp = nx.get_node_attributes(G, 'Expuestos')
     inf = nx.get_node_attributes(G, 'Infectados')
     rec = nx.get_node_attributes(G, 'Recuperados')
-    #print("Primero: ", sum(sus.values())+sum(exp.values())+sum(inf.values())+sum(rec.values()))
-    
+
     #Hago el ciclo sobre cada nodo haciendo el SIR
     for j in range(N):
         NS,NE,NI,NR = np.array(SIR.resolviendo(ti,tf,sus[j],exp[j],inf[j],rec[j],beta0,gamma0,sigma0))
-        #Le asigno los nuevos valores de cada variable al diccionario
-        sus[j] = NS[len(NS)-1]
-        exp[j] = NE[len(NE)-1]
-        inf[j] = NI[len(NI)-1]
-        rec[j] = NR[len(NR)-1]
-        
         #Imprimo los datos
-        net.print_data(j,NS,NE,NI,NR,param)
-        
+        net.print_data(j,NS,NE,NI,NR)
+        #Le asigno los nuevos valores de cada variable al diccionario
+        sus[j] = NS[-1]
+        exp[j] = NE[-1]
+        inf[j] = NI[-1]
+        rec[j] = NR[-1]
+    
     #Le asigno nuevamente los atributos
     nx.set_node_attributes(G, sus, 'Susceptibles')
     nx.set_node_attributes(G, exp, 'Expuestos')
     nx.set_node_attributes(G, inf, 'Infectados')
     nx.set_node_attributes(G, rec, 'Recuperados')
-
+    
     #Hago el intercambio entre los nodos
-    G = net.swap(G, matrix, 500)
-
-    #print("Tercero: ", sum(sus.values())+sum(exp.values())+sum(inf.values())+sum(rec.values()))
+    G = net.swap(G, matrix)
 
     #Dibujo la red con la propiedad de expuestos
-    #net.net_drawing(G,pos,exp,'Expuestos')
+    #net.net_drawing(G,pos,inf,'Infectados')
--- network.py
+++ network.py
@@ -1,7 +1,6 @@
 import networkx as nx
-import numpy as np
 import matplotlib.pyplot as plt
-import random as ran
+import numpy as np
 
 #Defino la función para dibujar la red
 def net_drawing(G,pos,labels,title):
@@ -12,7 +11,7 @@ def net_drawing(G,pos,labels,title):
     plt.clf()
 
 #Defino la función para hacer el intercambio de agentes
-def swap(G, matrix, maxi):
+def swap(G, matrix):
     #Hallo la suma de los pesos alrededor de cada nodo
     suma = []
     for i in range(len(matrix)):
@@ -37,16 +36,16 @@ def swap(G, matrix, maxi):
     arec = {}
     
     #Defino la proporción de personas de cada tipo que se van y las resto
-    value = 0
     for i in range(len(matrix)):
-        value = suma[i]/maxi
-        asus[i] = value*sus[i]
+        total = sus[i] + exp[i] + inf[i] + rec[i]
+        value = suma[i]
+        asus[i] = (sus[i]/total)*value
         sus[i] -= asus[i]
-        aexp[i] = value*exp[i]
+        aexp[i] = (exp[i]/total)*value
         exp[i] -= aexp[i]
-        ainf[i] = value*inf[i]
+        ainf[i] = (inf[i]/total)*value
         inf[i] -= ainf[i]
-        arec[i] = value*rec[i]
+        arec[i] = (rec[i]/total)*value
         rec[i] -= arec[i]
 
     #Hago la asignación de esas personas a los nuevos nodos
@@ -67,81 +66,8 @@ def swap(G, matrix, maxi):
 
     return G
 
-def print_data(i,NS,NE,NI,NR,k):
-    with open("Data_" + str(k) + "/node_" + str(i) + ".csv", "a") as myfile:
+#Esta función me imprime los datos de las características de los nodos
+def print_data(i,NS,NE,NI,NR):
+    with open("Data/node_" + str(i) + ".csv", "a") as myfile:
         for j in range(len(NS)):
             myfile.write(str(NS[j]) + '\t' + str(NE[j]) + '\t' + str(NI[j]) + '\t' + str(NR[j]) + '\n')
-
-'''
-Número de nodos
-N = 15
-
-Creo el grafo que va a estar en la mitad
-G = nx.watts_strogatz_graph(N,5,0.1,876)
-
-Genero la matriz de adjacencia como una matriz de numpy para editar.
-matrix = nx.to_numpy_matrix(G)
-print(matrix[0])
-
-Genero la matriz pesada
-ran.seed(68798)
-for i in range(N*N):
-    if(matrix.item(i) != 0):
-        matrix.itemset(i,ran.randint(1,11))
-
-Creo el nuevo grafo pesado
-G = nx.from_numpy_matrix(matrix)
-print(matrix)
-
-Defino la población en los nodos
-S = 4990
-I = 10
-
-Defino el diccionario que me va a dar la propiedad de Susceptibles, Expuesto, Infectado y Recuperado
-sus = {}
-exp = {}
-inf = {}
-rec = {}
-for i in range(N):
-    sus[i] = 0
-    exp[i] = 0
-    inf[i] = 0
-    rec[i] = 0
-Defino la semilla para el número aleatorio de susceptibles
-ran.seed(7678)
-Guardo cada Susceptible en algún nodo
-for i in range(S):
-    sus[ran.randint(0,N-1)] += 1
-print("Susceptibles: ", sus)
-Hago lo mismo para los Infectados
-ran.seed(673828)
-
-for i in range(I):
-    inf[ran.randint(0,N-1)] += 1
-print("Infectados: ", inf)
-    
-#Guardo los Susceptibles, Expuestos, Infectados y Recuperados como atributos del nodo
-nx.set_node_attributes(G, sus, 'Susceptibles')
-nx.set_node_attributes(G, exp, 'Expuestos')
-nx.set_node_attributes(G, inf, 'Infectados')
-nx.set_node_attributes(G, rec, 'Recuperados')    
-
-#Le doy una forma circular para poderlo ver mejor.
-pos = nx.circular_layout(G)
-
-net_drawing(G,pos,sus,'Susceptibles')
-
-#Hago el intercambio de personas
-G = swap(G, matrix, 500)
-sus = nx.get_node_attributes(G,'Susceptibles')
-exp = nx.get_node_attributes(G,'Expuestos')
-inf = nx.get_node_attributes(G,'Infectados')
-rec = nx.get_node_attributes(G,'Recuperados')
-
-
-#Dibujo la red
-net_drawing(G,pos,sus,'Susceptibles')
-#net_drawing(G,pos,exp,'Expuestos')
-#net_drawing(G,pos,inf,'Infectados')
-#net_drawing(G,pos,rec,'Recuperados')
-'''
